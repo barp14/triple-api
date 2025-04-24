2051292f4c71ecd8201853c5c000c861e99fa424
João Arthur Barp Begnini <joaobarpy@gmail.com>
2025-04-23 21:03:03 -0300

    criando tabela de campanhas

--- email-api/api-email.py
+++ email-api/api-email.py
@@ -1,10 +1,16 @@
 from flask import Flask
 import requests
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# URL da api de CRM
+# URL da API de CRM
 CRM_API_URL = 'http://localhost:3000/'
+
+# Configuração do MongoDB
+client = MongoClient('mongodb+srv://dbuser:DzbX3NP6MySS4ubt@cluster0.8x86g.mongodb.net/?retryWrites=true&w=majority')
+db = client['ecommerce']  # Nome do banco de dados
+enviados_collection = db['enviados']  # Nova coleção de "enviados"
 
 @app.route('/disparar-email', methods=['POST'])
 def disparar_email():
@@ -17,8 +23,16 @@ def disparar_email():
     for campanha in campanhas:
       print(f'E-mail enviado para: {cliente["email"]} com a campanha {campanha["nome"]}')
             
+    # Registrar no banco de dados os e-mails enviados
+      enviado = {
+          "email": cliente["email"],
+          "campanha": campanha["nome"],
+          "mensagem": f"Você está recebendo o e-mail da campanha: {campanha['nome']}.",
+          "status": "enviado"
+      }
+      enviados_collection.insert_one(enviado)
+            
   return "E-mails disparados com sucesso!"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
-	
